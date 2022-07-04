afea1c5b9b283b6476d9e8985c08d10269e5bdfa
mina kim <81088157+kimmina888@users.noreply.github.com>
2022-07-04 09:34:06 +0900

    Delete JavaBasic directory

--- JavaBasic/src/kr/hr_study/Hello.java
+++
@@ -1,5 +0,0 @@
-package kr.hr_study;
-
-public interface Hello {
-	public void SayHello();
-}
--- JavaBasic/src/kr/hr_study/HelloWorldEn.java
+++
@@ -1,9 +0,0 @@
-package kr.hr_study;
-
-public class HelloWorldEn implements Hello{
-
-	public void SayHello() {
-		System.out.println("Hi");
-	}
-	
-}

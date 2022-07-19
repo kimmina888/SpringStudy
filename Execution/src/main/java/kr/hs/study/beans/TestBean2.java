7a2dbd0440b7ad0dfd20dc759c47310aa822db08
mina kim <81088157+kimmina888@users.noreply.github.com>
2022-07-20 08:40:38 +0900

    Add files via upload

---
+++ Execution/src/main/java/kr/hs/study/beans/TestBean2.java
@@ -0,0 +1,8 @@
+package kr.hs.study.beans;
+
+public class TestBean2 {
+	public void method1() {
+		System.out.println("TestBean2ÀÇ method1()");
+	}
+
+}
---
+++ spring2/src/main/java/kr/hs/study/beans/Person.java
@@ -0,0 +1,8 @@
+package kr.hs.study.beans;
+
+public class Person {
+	public Person() {
+		System.out.println("person 의 기본생성자");
+	}
+
+}
